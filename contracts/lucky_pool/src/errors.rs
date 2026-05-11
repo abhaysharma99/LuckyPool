0ab2efc865e1101e7f8e0b2d4f723ed773177f46
Abhay Sharma <dsvv.abhi@gmail.com>
2026-05-11 13:45:00 +0530

    feat(contracts): define storage types and data structures

---
+++ contracts/lucky_pool/src/errors.rs
@@ -0,0 +1,16 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum LuckyPoolError {
+    AlreadyInitialized  = 1,
+    NotInitialized      = 2,
+    Paused              = 3,
+    Unauthorized        = 4,
+    InsufficientBalance = 5,
+    InsufficientPrize   = 6,
+    InvalidAmount       = 7,
+    NoDepositors        = 8,
+    FeeTooHigh          = 9,
+}
